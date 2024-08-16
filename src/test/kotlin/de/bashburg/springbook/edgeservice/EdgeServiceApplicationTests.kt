3964ef604118ffa13d23b430c1620f765eeee2d6
Christian Ambach <christian.ambach@codecentric.de>
2024-08-16 08:58:03 +0200

    initial commit

---
+++ src/test/kotlin/de/bashburg/springbook/edgeservice/EdgeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.bashburg.springbook.edgeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
