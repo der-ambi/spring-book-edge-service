9baae9f3f81d1378efc850dcee95acd49bcaa8e4
Christian Ambach <christian.ambach@codecentric.de>
2024-08-16 13:01:57 +0200

    9.4.1 add redis Testcontainers to SpringBootTest

--- src/test/kotlin/de/bashburg/springbook/edgeservice/EdgeServiceApplicationTests.kt
+++ src/test/kotlin/de/bashburg/springbook/edgeservice/EdgeServiceApplicationTests.kt
@@ -1,13 +1,23 @@
 package de.bashburg.springbook.edgeservice
 
+import com.redis.testcontainers.RedisContainer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
+    companion object {
+        @Container
+        @ServiceConnection
+        val redis = RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("7.4"))
+    }
 }
