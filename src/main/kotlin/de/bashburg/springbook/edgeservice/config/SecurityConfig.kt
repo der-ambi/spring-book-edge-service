98a794dc0ca61565eaf25f5269f6040817270df4
Christian Ambach <christian.ambach@codecentric.de>
2024-08-20 14:21:10 +0200

    11.1 add initial security

---
+++ src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package de.bashburg.springbook.edgeservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(serverHttpSecurity: ServerHttpSecurity): SecurityWebFilterChain =
+        serverHttpSecurity.authorizeExchange { exchange -> exchange.anyExchange().authenticated() }
+            .formLogin(Customizer.withDefaults())
+            .build()
+}
