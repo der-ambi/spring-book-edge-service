8ac2ea3b448afb368cea2dce01e6bd53c8127890
Christian Ambach <christian.ambach@codecentric.de>
2024-08-20 16:23:14 +0200

    11.4.5 configure logout to be propagated to keycloak

--- src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
+++ src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
@@ -4,13 +4,34 @@ import org.springframework.context.annotation.Bean
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
+import java.net.URI
 
 @EnableWebFluxSecurity
 class SecurityConfig {
     @Bean
-    fun springSecurityFilterChain(serverHttpSecurity: ServerHttpSecurity): SecurityWebFilterChain =
+    fun springSecurityFilterChain(
+        serverHttpSecurity: ServerHttpSecurity,
+        reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository
+    ): SecurityWebFilterChain =
         serverHttpSecurity.authorizeExchange { exchange -> exchange.anyExchange().authenticated() }
             .oauth2Login(Customizer.withDefaults())
+            .logout { logout ->
+                logout.logoutSuccessHandler(
+                    oidcLogoutSuccessHandler(
+                        reactiveClientRegistrationRepository
+                    )
+                )
+            }
             .build()
+
+    private fun oidcLogoutSuccessHandler(reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository): ServerLogoutSuccessHandler {
+        val oidcClientInitiatedServerLogoutSuccessHandler =
+            OidcClientInitiatedServerLogoutSuccessHandler(reactiveClientRegistrationRepository)
+        oidcClientInitiatedServerLogoutSuccessHandler.setLogoutSuccessUrl(URI("{baseUrl}"))
+        return oidcClientInitiatedServerLogoutSuccessHandler
+    }
 }
