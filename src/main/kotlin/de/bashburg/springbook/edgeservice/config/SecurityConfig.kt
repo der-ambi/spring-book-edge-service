24bf1b068acc24ba581b443a502f69c38c3f9367
Christian Ambach <christian.ambach@codecentric.de>
2024-08-21 15:41:58 +0200

    11.5.2 permit SPA and books requests without authentication

--- src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
+++ src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
@@ -2,12 +2,15 @@ package de.bashburg.springbook.edgeservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
 
 @Configuration
@@ -18,7 +21,19 @@ class SecurityConfig {
         serverHttpSecurity: ServerHttpSecurity,
         reactiveClientRegistrationRepository: ReactiveClientRegistrationRepository
     ): SecurityWebFilterChain =
-        serverHttpSecurity.authorizeExchange { exchange -> exchange.anyExchange().authenticated() }
+        serverHttpSecurity.authorizeExchange { exchange ->
+            exchange
+                .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                .anyExchange().authenticated()
+        }
+            .exceptionHandling { exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(
+                    HttpStatusServerEntryPoint(
+                        UNAUTHORIZED
+                    )
+                )
+            }
             .oauth2Login(Customizer.withDefaults())
             .logout { logout ->
                 logout.logoutSuccessHandler(
