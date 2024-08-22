5add5f92a87e66de5b18d42d1aedccaedc470b57
Christian Ambach <christian.ambach@codecentric.de>
2024-08-22 14:13:47 +0200

    12.1.1 store oauth tokens in redis to fix statefulness

--- src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
+++ src/main/kotlin/de/bashburg/springbook/edgeservice/config/SecurityConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler
@@ -53,4 +54,8 @@ class SecurityConfig {
         oidcClientInitiatedServerLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}")
         return oidcClientInitiatedServerLogoutSuccessHandler
     }
+
+    @Bean
+    fun authorizedClientRepository() = WebSessionServerOAuth2AuthorizedClientRepository()
+
 }
