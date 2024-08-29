ef80f542e98979593838a8bfbf2adcdcf7327f4e
Christian Ambach <christian.ambach@codecentric.de>
2024-08-29 14:18:36 +0200

    fix test

--- src/test/kotlin/de/bashburg/springbook/edgeservice/EdgeServiceApplicationTests.kt
+++ src/test/kotlin/de/bashburg/springbook/edgeservice/EdgeServiceApplicationTests.kt
@@ -3,13 +3,18 @@ package de.bashburg.springbook.edgeservice
 import com.redis.testcontainers.RedisContainer
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EdgeServiceApplicationTests {
+
+    @MockBean
+    lateinit var clientRegistrationRepository: ReactiveClientRegistrationRepository
 
     @Test
     fun contextLoads() {
